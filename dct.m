9681e71feac20c643b662f8e6498b39168864176
arjitarora26 <mailmearjitarora@gmail.com>
2019-11-18 13:48:53 +0530

    Deleted extra files

--- dct.m
+++
@@ -1,9 +0,0 @@
-function dct_image = dct_rgb(image)
-
-	for i = 1:3
-		dct_image(:,:,i) = dct2(image(:,:,i));
-	end
-
-	dct_image = uint8(dct_image);
-
-end
--- idct_rgb.matlab
+++
@@ -1,7 +0,0 @@
-function dct_image = idct_rgb(image)
-
-	for i = 1:3
-		dct_image(:,:,i) = idct2(image(:,:,i));
-    end
-
-end
