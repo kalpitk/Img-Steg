b6d9e663e79f574e1e43a06037c794c0a6c235c3
arjitarora26 <mailmearjitarora@gmail.com>
2019-11-28 00:19:39 +0530

    PM1 -> appending Length to secret message .     -> Function returning locations of changes

---
+++ PM1/appendLength_msg.m
@@ -0,0 +1,6 @@
+function newMsg = appendLength_msg(msg)
+    msg_len = length(msg);
+    str_len = dec2bin(msg_len);
+    zeros_append = zeros(1,16-length(str_len));
+    newMsg = [zeros_append SecMsg];
+end
---
+++ PM1/findLocations.m
@@ -0,0 +1,21 @@
+function change = findLocations(inpCoeff,secMsg)
+    updSecMsg = appendLength_msg(secMsg);
+    change = [];
+    inpIt=1;secIt=1;
+    
+    while secIt<=length(updSecMsg)
+        if inpCoeff(inpIt)>0
+            if mod(inpCoeff(inpIt)+updSecMsg(secIt),2)==1
+                   change = [change inpIt];
+            end
+            secIt = secIt+1;
+        elseif inpCoeff(inpIt)<0
+            if mod(inpCoeff(inpIt)+updSecMsg(secIt),2)==0
+                   change = [change inpIt];
+            end
+            secIt = secIt+1;
+        end
+        inpIt = inpIt+1;
+    end
+    
+end
