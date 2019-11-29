18f78117eb4f3b146a74bf6bfff295eda84e66ea
arjitarora26 <mailmearjitarora@gmail.com>
2019-11-29 18:01:31 +0530

    Random Data Generator

---
+++ PM1/getRandomData.m
@@ -0,0 +1,8 @@
+function randData = getRandomData(len)
+    randDataBinary = zeros(1,7*len);
+    for i = 1:len
+        ran = toBinary(floor(48 + (123-48)*rand()));
+        randDataBinary(1,7*(i-1)+1:(7*i)) = ran;
+    end
+    randData = toString(randDataBinary);
+end
--- PM1/toBinary.m
+++ PM1/toBinary.m
@@ -1,3 +1,3 @@
 function binString = toBinary(msg)
-binString = reshape(dec2bin(char(msg),7).'-'0',1,[])
+binString = reshape(dec2bin(char(msg),7).'-'0',1,[]);
 end
