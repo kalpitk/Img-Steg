f6e7c5697cf9ac58e909d7746d6e7fd97496dd3e
arjitarora26 <mailmearjitarora@gmail.com>
2019-11-29 17:32:43 +0530

    Initialize Population

---
+++ PM1/initPop.m
@@ -0,0 +1,10 @@
+function population = initPop(chrom_length,size_pop)
+    population = zeros(size_pop,chrom_length);
+    population(1,:) = ones(1,chrom_length);
+    population(2,:) = zeros(1,chrom_length);
+    for i = 3:size_pop
+        for j = 1:chrom_length
+            population(i,j) = round(rand());
+        end
+    end
+end
