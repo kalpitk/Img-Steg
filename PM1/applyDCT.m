df2d6b873fc9f8e780132118e5a1ddd06849feaf
kalpitk <kkothari1999@gmail.com>
2019-11-29 16:38:56 +0530

    Add DCT Code

--- PM1/applyDCT.m
+++ PM1/applyDCT.m
@@ -1,3 +1,22 @@
-function img = applyDCT(dct_img)
-	% Entropy Encoder goes here
+function [dct_quantized] = applyDCT(I)
+	I1=I;
+	[row coln]= size(I);
+	I= double(I);
+	I = I - (128*ones(512));
+
+	for i1=[1:8:row]
+	    for i2=[1:8:coln]
+	        zBLOCK=I(i1:i1+7,i2:i2+7);
+	        win1=DCT_matrix8*zBLOCK*iDCT_matrix8;
+	        dct_domain(i1:i1+7,i2:i2+7)=win1;
+	    end
+	end
+
+	for i1=[1:8:row]
+	    for i2=[1:8:coln]
+	        win1 = dct_domain(i1:i1+7,i2:i2+7);
+	        win2=round(win1./QX);
+	        dct_quantized(i1:i1+7,i2:i2+7)=win2;
+	    end
+	end
 end
--- PM1/toBinary.m
+++ PM1/toBinary.m
@@ -1,3 +1,10 @@
-function b = toBinary(msg)
-	b = reshape(dec2bin(s, 8).'-'0',1,[])
+function res = toBinary(msg)
+	b = dec2bin(msg,8);
+    len = length(msg);
+    
+    res = zeros(len*8);
+    
+    for i = 1:len
+        res(i:i+8) = b(i);
+    end
 end
